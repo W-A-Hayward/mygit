a4630c3f710fb54d0acc31372e6357e4ab9921bc
W-A-Hayward <whaywardca@gmail.com>
2026-06-01 23:42:47 -0400

    feat: take a file as arg, hash it, compress it and store in mygit dir

---
+++ hash-object.c
@@ -0,0 +1,12 @@
+// hash-object  →  write-tree  →  commit-tree  →  log  →  checkout
+
+// given a -w flag (optional, defaults to false), and file path
+// determine size
+
+// determine content
+
+// SHA-1 <type> <size>\0<content> to 40 hex string. type = blob | tree | commit
+
+// z-lib compression
+
+// storing using the 2-38 split

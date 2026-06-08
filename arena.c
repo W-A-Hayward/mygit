e8f0051736ad1b4dd941f28b3160ca1c61287f3e
W-A-Hayward <whaywardca@gmail.com>
2026-06-07 23:14:54 -0400

    feat: finished arena

--- arena.c
+++ arena.c
@@ -6,10 +6,12 @@ Arena arena_init(size_t capacity)
   arena.buffer = (uint8_t *)malloc(capacity);
   arena.capacity = capacity;
   arena.offset = 0;
+  arena.count = 0;
+
   return arena;
 }
 
-static inline uintptr_t align_forward(uintptr_t ptr, size_t align)
+uintptr_t align_forward(uintptr_t ptr, size_t align)
 {
   uintptr_t modulo = ptr % align;
   if (modulo != 0)
@@ -18,4 +20,51 @@ static inline uintptr_t align_forward(uintptr_t ptr, size_t align)
   return ptr;
 }
 
-void *arena_alloc(Arena *arena, size_t size, size_t alignment) {}
+void dyn_alloc(Arena *arena)
+{
+  size_t new_cap =
+      arena->capacity == 0 ? 1024 : (size_t)((double)arena->capacity * 1.5);
+  uint8_t *dest = (uint8_t *)realloc(arena->buffer, new_cap);
+
+  if (dest == NULL)
+  {
+    perror("Arena reallocation failed");
+    exit(EXIT_FAILURE);
+  }
+
+  arena->buffer = dest;
+  arena->capacity = new_cap;
+}
+
+void *arena_alloc(Arena *arena, size_t size, size_t alignment)
+{
+  // align current pointer to next word start
+  uintptr_t curr_ptr = (uintptr_t)arena->buffer + arena->offset;
+  uintptr_t aligned_ptr = align_forward(curr_ptr, alignment);
+
+  // pad the size to go to next word start
+  size_t padding = aligned_ptr - curr_ptr;
+  size_t total_size = size + padding;
+
+  if (arena->offset + total_size > arena->capacity)
+  {
+    dyn_alloc(arena);
+  }
+
+  void *ptr = (void *)aligned_ptr;
+  arena->offset += total_size;
+  arena->count++;
+
+  return ptr;
+}
+
+void arena_reset(Arena *arena) { arena->offset = 0; }
+
+void arena_free(Arena *arena)
+{
+  free(arena->buffer);
+  arena->buffer = NULL;
+  arena->capacity = 0;
+  arena->offset = 0;
+  arena->count = 0;
+}
--- arena.h
+++ arena.h
@@ -2,16 +2,19 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct
 {
   uint8_t *buffer;
   size_t capacity;
   size_t offset;
+  size_t count;
 } Arena;
 
 Arena arena_init(size_t capacity);
-static inline uintptr_t align_forward(uintptr_t ptr, size_t align);
+uintptr_t align_forward(uintptr_t ptr, size_t align);
+void dyn_alloc(Arena *arena);
 void *arena_alloc(Arena *arena, size_t size, size_t alignment);
 void arena_reset(Arena *arena);
 void arena_free(Arena *arena);

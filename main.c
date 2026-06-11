a3f8b98d87b0a84a057480e62bd045b86d9dfb87
W-A-Hayward <whaywardca@gmail.com>
2026-06-10 23:11:30 -0400

    feat: impl commands and init helper

--- main.c
+++ main.c
@@ -1,14 +1,51 @@
 // hash-object  →  write-tree  →  commit-tree  →  log  →  checkout
 #include "hash_object.h"
+#include "init.h"
 #include "write_tree.h"
 
-int main(int argc, char *argv[])
+typedef int (*command_fn)(int argc, char *argv[]);
+
+typedef struct
+{
+  const char *name;
+  command_fn fn;
+} Command;
+
+static void usage(void)
+{
+  fprintf(stderr, "usage: mygit <command> [<args>...]\n"
+                  "commands: init, hash-object, write-tree\n");
+}
+
+static int cmd_init(int argc, char *argv[])
 {
   (void)argc;
   (void)argv;
+  return init() == 0 ? 0 : 1;
+}
 
-  unsigned char hash[20];
-  write_tree("./test", hash);
+static const Command commands[] = {
+    {"init", cmd_init},
+    // {"hash-object", cmd_hash_object},
+    // {"write-tree", cmd_write_tree},
+    {NULL, NULL},
+};
 
-  return 0;
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+  {
+    usage();
+    return 1;
+  }
+
+  for (const Command *c = commands; c->name; c++)
+  {
+    if (strcmp(argv[1], c->name) == 0)
+      return c->fn(argc, argv);
+  }
+
+  fprintf(stderr, "unknown command: %s\n", argv[1]);
+  usage();
+  return 1;
 }
